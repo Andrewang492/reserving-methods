88fd568eaf95727aa80f20ddbf226fcece2fb0bf
Andre Wang on his desktop <andrewang87@yahoo.com.au>
2024-06-17 18:48:27 +1000

    begin tenure runoff analysis

--- script1.R
+++ script1.R
@@ -19,3 +19,46 @@ policies %>%
   filter(month(claim_loss_date) != month(accident_month) |
   (year(claim_loss_date) != year(accident_month)))
 
+claims  <- policies %>% filter(!is.na(total_claims_cost) & !is.na(claim_loss_date))
+
+#----  show that sum insured is always more than the total claims costs
+
+policies %>% 
+  mutate(total_claims_payment = ifelse(is.na(policies$total_claims_cost), 0, policies$total_claims_cost)) %>%
+  select(policy_id, sum_insured, total_claims_payment) %>% 
+  filter(total_claims_payment != 0) %>%
+  group_by(policy_id, sum_insured) %>%
+  summarise(total_claims = sum(total_claims_payment)) %>%
+  filter(total_claims > sum_insured) # this code wrongly groups things.
+  
+# apparently not:
+policies %>% 
+  select(policy_id, sum_insured, total_claims_cost) %>% 
+  filter(!is.na(total_claims_cost)) %>%
+  filter(total_claims_cost > sum_insured) 
+
+policies %>% 
+  select(policy_id, sum_insured, total_claims_cost) %>% 
+  filter(!is.na(total_claims_cost)) %>%
+  filter(total_claims_cost<sum_insured) 
+
+
+# policies exposure 0 (another claim in a month)
+View(policies %>% filter(exposure == 0) %>% group_by(policy_id) %>% select(policy_id) %>% summarise(n= n()))
+
+# policy each record data
+# how much each policy paid and for what exposure length.
+policies %>%
+  mutate(total_claims_payment = ifelse(is.na(policies$total_claims_cost), 0, policies$total_claims_cost)) %>%
+  group_by(policy_id) %>%
+  summarise(total_payment = sum(total_claims_payment), total_exposure_days = sum(exposure_days), 
+            start_date = min(term_start_date)) #assume policies are continuous (they are currently split up into years.)
+
+# analysis on tenure
+
+claims %>% 
+  mutate(year = year(term_start_date)) %>%
+  group_by(policy_tenure, year) %>%
+  summarise(total_cost = sum(total_claims_cost))
+
+
